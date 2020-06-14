2a20251b2a5a77026d2445ce400f9045d0235a06
Adam Wawro <adamwawro@wp.pl>
2020-06-14 12:56:05 -0700

    addad turnover maps, reformatted powiaty names

--- R/districts_plot.R
+++ R/districts_plot.R
@@ -10,8 +10,33 @@ results_candidate_clean <- readRDS("output/clean/results_candidate_clean.rds")
 results_slate_clean <- readRDS("output/clean/results_slate_clean.rds")
 results_candidate_full <- readRDS("output/clean/results_candidate_full.rds")
 map_powiaty <- readRDS("output/clean/map_powiaty.rds")
-map_wojwodztwa <- readRDS("output/clean/map_wojewodztwa.rds")
+map_wojewodztwa <- readRDS("output/clean/map_wojewodztwa.rds")
 
+#turnover wojewodztwa
+turnover_woj <- results_station_clean %>%
+  group_by(wojewodztwo) %>%
+  summarize(turnover = sum(wydane_karty) / sum(wyborcow_uprawnionych) * 100)
+
+map_wojewodztwa %>%
+  left_join(turnover_woj, by = c("nazwa" = "wojewodztwo")) %>%
+  ggplot() +
+    geom_sf(aes(fill = turnover)) +
+    scale_fill_viridis(limits = c(45,80))
+
+#turnover powiaty
+turnover_pow <- results_station_clean %>%
+  group_by(powiat) %>%
+  summarize(turnover = sum(wydane_karty) / sum(wyborcow_uprawnionych) * 100)
+
+
+map_powiaty %>%
+  left_join(turnover_pow, by = c("nazwa" = "powiat")) %>%
+  ggplot() +
+  geom_sf(aes(fill = turnover)) +
+  scale_fill_viridis(limits = c(45,80))
+
+
+## tests
 results_station_clean %>%
   filter(typ_obszaru %in% c("miasto", "wieś")) %>%
   ggplot(aes(x=wojewodztwo, y=(niewykorzystane_karty/otrzymanych_kart), fill = typ_obszaru)) +
@@ -30,11 +55,9 @@ results_station_clean %>%
 results_station_clean %>%
   filter(typ_obszaru %in% c("miasto", "wieś")) %>%
   ggplot(aes(x = wyborcow_uprawnionych, y = (wydane_karty / wyborcow_uprawnionych))) +
-    stat_density2d(data = results_station_clean[results_station_clean$typ_obszaru == "miasto",], color = "blue") +
-    stat_density2d(data = results_station_clean[results_station_clean$typ_obszaru == "wieś",], color = "red") +
-    #geom_point(alpha = 0.05, size = 0.1) +
-    #geom_smooth(method = "lm", se = FALSE, color = "gray60", size = 0.5) +
+    stat_density2d(data = results_station_clean[results_station_clean$typ_obszaru == "miasto",], color = "blue", alpha = 0.5) +
+    stat_density2d(data = results_station_clean[results_station_clean$typ_obszaru == "wieś",], color = "red", alpha= 0.5) +
     labs(x = "voters per station", y = "turnover") +
     expand_limits(y = c(0,1)) +
-    theme_amw() +
+    #theme_bw() +
     facet_wrap("wojewodztwo")
