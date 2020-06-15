d86df7d606ef8ea0592f4b577236647a249e7ab8
Adam Wawro <adamwawro@wp.pl>
2020-06-14 20:58:45 -0700

    fixed merge issues

---
+++ R/turnover_plot.R
@@ -0,0 +1,67 @@
+library(tidyverse)
+library(ggridges)
+library(viridis)
+library(sf)
+
+cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+results_station_clean <- readRDS("output/clean/results_station_clean.rds")
+results_candidate_clean <- readRDS("output/clean/results_candidate_clean.rds")
+results_slate_clean <- readRDS("output/clean/results_slate_clean.rds")
+results_candidate_full <- readRDS("output/clean/results_candidate_full.rds")
+map_powiaty <- readRDS("output/clean/map_powiaty.rds")
+map_wojewodztwa <- readRDS("output/clean/map_wojewodztwa.rds")
+
+#turnover wojewodztwa
+turnover_woj <- results_station_clean %>%
+  group_by(wojewodztwo) %>%
+  summarize(turnover = sum(wydane_karty) / sum(wyborcow_uprawnionych) * 100, .groups = 'drop')
+
+map_wojewodztwa %>%
+  left_join(turnover_woj, by = c("nazwa" = "wojewodztwo")) %>%
+  ggplot() +
+    geom_sf(aes(fill = turnover), size = 0.1, color = "white") +
+    scale_fill_viridis(limits = c(46,78), breaks = c(50, 55, 60, 65, 70, 75), name = "voter turnover / %", option = "E") +
+    theme_void() +
+    theme(legend.position = "right")
+
+#turnover powiaty
+turnover_pow <- results_station_clean %>%
+  group_by(powiat) %>%
+  summarize(turnover = sum(wydane_karty) / sum(wyborcow_uprawnionych) * 100, .groups = 'drop')
+
+map_powiaty %>%
+  left_join(turnover_pow, by = c("nazwa" = "powiat")) %>%
+  ggplot() +
+    geom_sf(aes(fill = turnover)) +
+    geom_sf(data = map_wojewodztwa, size = 0.1, fill = NA, color = "white") +
+    scale_fill_viridis(limits = c(46,78), breaks = c(50, 55, 60, 65, 70, 75), name = "voter turnover / %", option = "E") +
+    theme_void() +
+    theme(legend.position = "right")
+
+
+## tests
+results_station_clean %>%
+  filter(typ_obszaru %in% c("miasto", "wieś")) %>%
+  ggplot(aes(x=wojewodztwo, y=(niewykorzystane_karty/otrzymanych_kart), fill = typ_obszaru)) +
+  stat_boxplot(outlier.shape = NA) +
+  labs(y="% unused ballots", x="voivodeship") +
+  coord_flip()
+
+results_station_clean %>%
+  filter(typ_obszaru %in% c("miasto", "wieś")) %>%
+  ggplot(aes(y = wojewodztwo, x = wyborcow_uprawnionych, fill = typ_obszaru)) +
+  geom_density_ridges(draw_baseline = FALSE, alpha = 0.5) +
+  labs(x = "voters per station", y = "voivodeship", fill = "area type") +
+  theme(legend.position = "bottom") +
+  scale_color_manual(values = cbPalette[c(3,2)])
+
+results_station_clean %>%
+  filter(typ_obszaru %in% c("miasto", "wieś")) %>%
+  ggplot(aes(x = wyborcow_uprawnionych, y = (wydane_karty / wyborcow_uprawnionych))) +
+    stat_density2d(data = results_station_clean[results_station_clean$typ_obszaru == "miasto",], color = "blue", alpha = 0.5) +
+    stat_density2d(data = results_station_clean[results_station_clean$typ_obszaru == "wieś",], color = "red", alpha= 0.5) +
+    labs(x = "voters per station", y = "turnover") +
+    expand_limits(y = c(0,1)) +
+    #theme_bw() +
+    facet_wrap("wojewodztwo")
